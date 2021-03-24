1e658218bf80964d4abbe8c68836557746d73a98
duchaoyu <duch@student.ethz.ch>
2021-03-24 21:19:43 +0100

    - add blender export.py - add fracture model 6 pieces, 20 pieces

---
+++ data_processing/import_mesh.py
@@ -0,0 +1,8 @@
+import os
+from compas.datastructures import Mesh
+
+# ==============================================================================
+# File
+# ==============================================================================
+HERE = os.path.dirname(__file__)
+FILE = os.path.join(HERE, 'data', 'cablenet.json')
