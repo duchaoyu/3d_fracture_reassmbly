24a40cb07da57f4a3aafb8b2d026f9d4860a2456
duchaoyu <duch@student.ethz.ch>
2021-03-24 21:44:31 +0100

    - add viewer, read all freature elements and vis in different colors

--- data_processing/import_mesh.py
+++ data_processing/import_mesh.py
@@ -1,8 +1,30 @@
 import os
 from compas.datastructures import Mesh
+from compas.utilities import i_to_rgb
+
+from compas_view2.app import App
 
 # ==============================================================================
 # File
 # ==============================================================================
-HERE = os.path.dirname(__file__)
-FILE = os.path.join(HERE, 'data', 'cablenet.json')
+HERE = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+FILE_FOLDER = os.path.join(HERE, 'data', 'cube_6')
+# FILE_I = os.path.join(HERE, 'data', 'cube_6/Cube_shard.obj')
+
+# initialize viewer
+viewer = App()
+
+file_nums = len(os.listdir(FILE_FOLDER))
+print(file_nums)
+for i, filename in enumerate(os.listdir(FILE_FOLDER)):
+    if filename.endswith(".obj"): 
+        FILE_I = os.path.join(FILE_FOLDER, filename)
+        mesh = Mesh.from_obj(FILE_I)
+        viewer.add(mesh, facecolor=i_to_rgb(i/file_nums, True))
+
+
+
+# ==============================================================================
+# Viz
+# ==============================================================================
+viewer.run()
