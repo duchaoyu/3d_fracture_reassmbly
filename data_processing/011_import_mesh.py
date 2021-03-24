5ce375c4cfa72aa05c1c219e0bebf29a3c32204a
duchaoyu <duch@student.ethz.ch>
2021-03-24 21:47:19 +0100

    - visaulize single mesh

--- data_processing/011_import_mesh.py
+++ data_processing/011_import_mesh.py
@@ -9,22 +9,18 @@ from compas_view2.app import App
 # ==============================================================================
 HERE = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 FILE_FOLDER = os.path.join(HERE, 'data', 'cube_6')
-# FILE_I = os.path.join(HERE, 'data', 'cube_6/Cube_shard.obj')
+FILE_I = os.path.join(HERE, 'data', 'cube_6/Cube_shard.obj')
 
-# initialize viewer
-viewer = App()
 
-file_nums = len(os.listdir(FILE_FOLDER))
-print(file_nums)
-for i, filename in enumerate(os.listdir(FILE_FOLDER)):
-    if filename.endswith(".obj"): 
-        FILE_I = os.path.join(FILE_FOLDER, filename)
-        mesh = Mesh.from_obj(FILE_I)
-        viewer.add(mesh, facecolor=i_to_rgb(i/file_nums, True))
-
+# ==============================================================================
+# Mesh
+# ==============================================================================
+mesh = Mesh.from_obj(FILE_I)
 
 
 # ==============================================================================
 # Viz
 # ==============================================================================
+viewer = App()
+viewer.add(mesh)
 viewer.run()
