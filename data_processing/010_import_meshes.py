7a49a1adcca2aa88441e30b817c699d6bb58a23a
duchaoyu <duch@student.ethz.ch>
2021-04-14 21:08:45 +0200

    - convert mesh to .oFF - clean the repo

--- data_processing/010_import_meshes.py
+++ data_processing/010_import_meshes.py
@@ -8,7 +8,7 @@ from compas_view2.app import App
 # File
 # ==============================================================================
 HERE = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-FILE_FOLDER = os.path.join(HERE, 'data', 'cat_seed_0')
+FILE_FOLDER = os.path.join(HERE, 'data', 'cat_seed_1')
 # FILE_I = os.path.join(HERE, 'data', 'cube_6/Cube_shard.obj')
 
 # initialize viewer
@@ -16,12 +16,22 @@ viewer = App()
 
 file_nums = len(os.listdir(FILE_FOLDER))
 print(file_nums)
+
+# calculate the total vertices
+total_vertices = 0
 for i, filename in enumerate(os.listdir(FILE_FOLDER)):
     if filename.endswith(".obj"): 
+        FILE_I = os.path.join(FILE_FOLDER, filename)
+        mesh = Mesh.from_obj(FILE_I)
         if "shard" in filename:
-            FILE_I = os.path.join(FILE_FOLDER, filename)
-            mesh = Mesh.from_obj(FILE_I)
+            len_vertices = len(list(mesh.vertices()))
+            print(len_vertices)
+            total_vertices += len_vertices
             viewer.add(mesh, facecolor=i_to_rgb(i/file_nums, True))
+        else:
+            print("Input mesh vertices:", len(list(mesh.vertices())))
+
+print("Total fracture vertices:", total_vertices)
 
 # ==============================================================================
 # Viz
--- data_processing/012_transform.py
+++ data_processing/012_transform.py
@@ -11,8 +11,9 @@ from compas_view2.app import App
 # ==============================================================================
 HERE = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 FILE_FOLDER = os.path.join(HERE, 'data', 'cat_seed_1')
-FILE_I = os.path.join(HERE, 'data', 'cat_seed_1/grp1.001_shard.004.obj')
-FILE_O = os.path.join(HERE, 'data', 'cat_seed_1/grp1.001_shard.004.off')
+file_name = "cat_seed_1/grp1.001_shard.004"
+FILE_I = os.path.join(HERE, 'data', '%s.obj' % file_name)
+FILE_O = os.path.join(HERE, 'data', '%s_transformed.off' % file_name)
 
 # ==============================================================================
 # Mesh
@@ -22,13 +23,24 @@ centorid = mesh.centroid()
 T = Translation.from_vector([a * -1 for a in centorid])
 mesh_transform_numpy(mesh, T)
 
-mesh.to_off(FILE_O, author="Chaoyu")
+# # get vertex key and coordinates
+# for vkey in mesh.vertices():
+#     xyz = mesh.vertex_coordinates(vkey)
+#     print('vertex key: ', vkey, 'coordinate: ', xyz)
+
+# transform mesh
+T = matrix_from_axis_and_angle([0, 0.5, 0.3], m.pi / 4)
+# create a copy of the original mesh 
+mesh_t = mesh.copy()
+mesh_transform_numpy(mesh_t, T)
+
+mesh_t.to_off(FILE_O, author="Chaoyu")
 # ==============================================================================
 # Viz
 # ==============================================================================
 viewer = App()
 # only show points
-# viewer.add(mesh, show_points=True, show_lines=False, show_faces=False)
+# viewer.add(mesh_t, show_points=True, show_lines=False, show_faces=False)
 # show the mesh
-viewer.add(mesh)
+viewer.add(mesh_t)
 viewer.run()
